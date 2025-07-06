2a24a6d42ed96379320ea8ac167bd30d639687d9
Tecken7 <v.hartl@seznam.cz>
2025-07-06 05:13:18 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body style={{ margin: 0, background: "black" }}>{children}</body>
+            <body style={{ margin: 0, background: "black" }}>
+                {children}
+            </body>
         </html>
     );
 }
